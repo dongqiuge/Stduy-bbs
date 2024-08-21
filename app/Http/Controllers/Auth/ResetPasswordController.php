a6494385818ec67a24b9821de5d54bc004eb0420
wang <1234>
2024-08-22 00:05:20 +0800

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +31,17 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * 重写 auth 脚手架密码重置成功后的响应
+     *
+     * @param Request $request
+     * @param $response
+     * @return Application|RedirectResponse|Redirector
+     */
+    public function sendResetResponse(Request $request, $response): Redirector|RedirectResponse|Application
+    {
+        session()->flash('success', '密码更新成功，您已成功登录！');
+        return redirect($this->redirectPath());
+    }
 }
