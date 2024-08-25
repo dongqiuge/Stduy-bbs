dc45a62d4c905e4194400a574d598b16f7371aa3
wang <1234>
2024-08-26 07:59:35 +0800

    生成话题骨架

---
+++ app/Models/Topic.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Topic extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * 定义于 category 的关联
+     *
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * 定义于 user 的关联
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/seeders/TopicsTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Topic;
+
+class TopicsTableSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Topic::factory()->count(115)->create();
+    }
+}
+
