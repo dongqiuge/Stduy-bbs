e23f9de3e4a093467cf0ba16638bad4ceec1de2a
wang <1234>
2024-08-26 23:59:31 +0800

    修复N+1问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,57 +2,119 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\TopicRequest;
 use App\Models\Topic;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TopicRequest;
 
 class TopicsController extends Controller
 {
+
+    /**
+     * TopicsController constructor.
+     */
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-		$topics = Topic::paginate();
-		return view('topics.index', compact('topics'));
-	}
+    /**
+     * 显示话题列表
+     * 用 with() 方法预加载了话题数据的用户数据和分类数据，预加载是为了避免 N+1 问题
+     *
+     * @param Request $request
+     * @param Topic $topic
+     * @return View|Factory|Application
+     */
+    public function index(Request $request, Topic $topic): View|Factory|Application
+    {
+        $topics = $topic->withOrder($request->order)
+            ->with('user', 'category') // 预加载 user 和 category 关联，避免 N+1 问题
+            ->paginate(20);
+        return view('topics.index', compact('topics'));
+    }
 
-    public function show(Topic $topic)
+    /**
+     * 显示话题详情
+     *
+     * @param Topic $topic
+     * @return Application|Factory|View
+     */
+    public function show(Topic $topic): Factory|View|Application
     {
         return view('topics.show', compact('topic'));
     }
 
-	public function create(Topic $topic)
-	{
-		return view('topics.create_and_edit', compact('topic'));
-	}
+    /**
+     * 显示创建话题页面
+     *
+     * @param Topic $topic
+     * @return Factory|View|Application
+     */
+    public function create(Topic $topic): Factory|View|Application
+    {
+        return view('topics.create_and_edit', compact('topic'));
+    }
 
-	public function store(TopicRequest $request)
-	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
-	}
+    /**
+     * 创建话题
+     *
+     * @param TopicRequest $request
+     * @return RedirectResponse
+     */
+    public function store(TopicRequest $request): RedirectResponse
+    {
+        $topic = Topic::create($request->all());
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+    }
 
-	public function edit(Topic $topic)
-	{
+
+    /**
+     * 显示编辑话题页面
+     *
+     * @param Topic $topic
+     * @return Factory|View|Application
+     * @throws AuthorizationException
+     */
+    public function edit(Topic $topic): Factory|View|Application
+    {
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
-	}
+        return view('topics.create_and_edit', compact('topic'));
+    }
 
-	public function update(TopicRequest $request, Topic $topic)
-	{
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
+    /**
+     * 更新话题
+     *
+     * @param TopicRequest $request
+     * @param Topic $topic
+     * @return RedirectResponse
+     * @throws AuthorizationException
+     */
+    public function update(TopicRequest $request, Topic $topic): RedirectResponse
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
-	}
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+    }
 
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
+    /**
+     * 删除话题
+     *
+     * @param Topic $topic
+     * @return RedirectResponse
+     * @throws AuthorizationException
+     */
+    public function destroy(Topic $topic): RedirectResponse
+    {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
-	}
+        return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+    }
 }
