df66353a8d08a4cd29166b59d1c2c90fcd3d801e
wang <1234>
2024-08-28 08:05:34 +0800

    用户的话题列表

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,18 +2,76 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Model extends EloquentModel
+class Topic extends Model
 {
-    public function scopeRecent($query)
+    use HasFactory;
+
+    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * 定义于 category 的关联
+     *
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
     {
-        return $query->orderBy('id', 'desc');
+        return $this->belongsTo(Category::class);
     }
 
-    public function scopeOrdered($query)
+    /**
+     * 定义于 user 的关联
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
     {
-        return $query->orderBy('order', 'desc');
+        return $this->belongsTo(User::class);
     }
 
+    /**
+     * 话题排序
+     *
+     * @param $query
+     * @param $order
+     */
+    public function scopeWithOrder($query, $order): void
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+    }
+
+    /**
+     * 话题排序：最后回复时间排序
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeRecentReplied($query): mixed
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性.
+        // 此时会自动触发框架对数据模型的更新，更新的字段会自动同步到数据库中。
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    /**
+     * 话题排序：创建时间排序
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeRecent($query): mixed
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -43,4 +44,12 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function topics(): HasMany
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
