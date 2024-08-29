a3e79691cf97847992fef41e01e52afa47b44331
wang <1234>
2024-08-29 08:09:48 +0800

    修复XSS注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -17,6 +17,9 @@ class TopicObserver
      */
     public function saving(Topic $topic): void
     {
+        // 使用 mews/purifier 来过滤内容，防止 XSS 攻击
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
